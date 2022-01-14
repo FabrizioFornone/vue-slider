40816251c132e96f3f00130ccc403c0391762905
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-14 14:22:52 +0100

    Repo Created

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+/* Partendo dal markup qui in allegato (di cui in passato abbiamo
+ fatto una versione simile in js plain), rifare lo slider ma
+  questa volta usando Vue. */
+
+new Vue({
+  el: "#app",
+  data: {
+      test: "Ecco il test"
+  },
+  methods:{
+
+  }
+});
